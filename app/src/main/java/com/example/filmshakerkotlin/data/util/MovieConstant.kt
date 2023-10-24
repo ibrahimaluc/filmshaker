e128d746b824402e01a1926515a3f0d6418e875e
ibrahimaluc <ibrahimaluc@ogr.iu.edu.tr>
2023-10-25 01:08:49 +0300

    build.gradle changed

--- app/src/main/java/com/example/filmshakerkotlin/data/util/MovieConstant.kt
+++ app/src/main/java/com/example/filmshakerkotlin/data/util/MovieConstant.kt
@@ -8,7 +8,7 @@ object MovieConstant {
     const val BASE_URL = BuildConfig.BASE_URL
     const val API_KEY = BuildConfig.API_KEY
     const val LANGUAGE = BuildConfig.LANGUAGE
-    const val ADD_APP_PROD_KEY = BuildConfig.ADD_APP_PROD_KEY
+    const val ADD_KEY = BuildConfig.ADD_KEY
     var randomNum = Random.nextInt(1, 501)
 
 
